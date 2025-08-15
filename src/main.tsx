e650d12eb3160628d87033a834cb88c290aa0b84
BP-H <blueprinthustler@gmail.com>
2025-08-15 00:28:36 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// If you have global styles (from earlier step), keep this:
-import "./styles.css";
-
-// Register the <model-viewer> web component in the browser only.
-if (typeof window !== "undefined") {
-  import("@google/model-viewer");
-}
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
